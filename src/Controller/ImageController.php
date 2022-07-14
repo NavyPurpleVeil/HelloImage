1ca5d8ba059999c3b144d51edaa4da06da95766b
Jakub Strzelecki <kubast2@pop-os.localdomain>
2022-07-14 14:53:43 +0200

    Use FindByParameter instead of FindBy

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -79,7 +79,7 @@ class ImageController extends AbstractController {
 		}
 
 		$authKey = $cookies["auth"];
-		$user = $UserRep->findBy(array(),array('$auth_key'=>$authKey));
+		$user = $UserRep->findByParameter(array('auth_key'=>$authKey));
 		if($user == NULL) { // what if it doesn't return NULL?
 			return $this->authFailureResp($UserRep, $doctrine);
 		}
@@ -165,7 +165,7 @@ class ImageController extends AbstractController {
 		}
 
 		$authKey = $cookies["auth"];
-		$user = $UserRep->findBy(array(),array('$auth_key'=>$authKey));
+		$user = $UserRep->findByParameter(array('auth_key'=>$authKey));
 		if($user == NULL) {
 			return $this->authFailureResp($UserRep, $doctrine);
 		}
@@ -229,7 +229,7 @@ class ImageController extends AbstractController {
 		}
 
 		$authKey = $cookies["auth"];
-		$user = $UserRep->findBy(array(),array('$auth_key'=>$authKey));
+		$user = $UserRep->findByParameter(array('auth_key'=>$authKey));
 		if($user == NULL) {
 			return $this->authFailureResp($UserRep, $doctrine);
 		}
@@ -264,7 +264,7 @@ class ImageController extends AbstractController {
 		}
 
 		$authKey = $cookies["auth"];
-		$user = $UserRep->findBy(array(),array('$auth_key'=>$authKey));
+		$user = $UserRep->findByParameter(array('auth_key'=>$authKey));
 		if($user == NULL) {
 			return $this->authFailureResp($UserRep, $doctrine);
 		}
@@ -295,7 +295,7 @@ class ImageController extends AbstractController {
 		}
 
 		$authKey = $cookies["auth"];
-		$user = $UserRep->findBy(array(),array('$auth_key'=>$authKey));
+		$user = $UserRep->findByParameter(array('auth_key'=>$authKey));
 		if($user == NULL) {
 			return $this->authFailureResp($UserRep, $doctrine);
 		}
